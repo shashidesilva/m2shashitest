1a53ac80e61b12c100dadca884c19e8cc9ef1a51
shashi <s.desilva@ism-apac.com>
2017-04-04 13:22:27 +0530

    home page slider

---
+++ html/app/design/frontend/Aneue/Theme/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    map: {
+        '*': {
+            'owlcarousel' : 'js/owl-carousel/owl.carousel'
+        }
+    },
+    shim: {
+        "owlcarousel":["jquery"]
+    }
+};
+
